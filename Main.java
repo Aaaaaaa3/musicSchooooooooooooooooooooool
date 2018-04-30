157aee0b7b9fd013142078fbca05d5f2aa7dfe20
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-04-30 11:18:19 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
 /**
- * Auto Generated Java Class.
+ * Read in the file
+ * Add and remove students
  */
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
   
   
   public static void main(String[] args) { 
     
+    try {
+      File file = new File("Tasha.txt");
+      Scanner input = new Scanner(file);
+      
+      while (input.hasNext()){
+        System.out.println(input.nextLine());
+      }
+    }
+    catch(Exception E){
+      System.out.println("Error 1");
+    }
   }
-  
-  /* ADD YOUR CODE HERE */
   
 }
