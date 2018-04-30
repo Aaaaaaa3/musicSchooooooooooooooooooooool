a0bcf86f57d5347ebfd5e1d6503fe21b4184f0d8
chesyrin <chesyrin@users.noreply.github.com>
2018-04-30 10:42:34 -0400

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,10 @@
+//gui for project
+import javax.swing.*;
+public class GUI {
+  public static void main(String[] args) { 
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400,400);
+    frame.setVisible (true);
+  }  
+}
