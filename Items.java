45f6f2ab74ff35079e5c52bf75b4d77b1e5eb02b
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-04-30 10:50:56 -0400

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,15 +1,54 @@
 /**
  * Template for insturments, equipment and sheet music
  */
-public abstract class Items {
-  boolean condition;
-  String name;
-  String descr;
-  String num;
+public class Items {
+  private boolean condition;
+  private String name;
+  private String descr;
+  private String num;
   
   String stu = "";
   String dueDate;//change into appropriate thing later
   
+  /********************************************************* the constructors ***********************************************/
+  //default constructor
   public Items() { 
     `}
+  
+  public Items(boolean condition, String name, String description, String num){
+    this.condition = condition;
+    this.name = name;
+    this.descr = description;
+    this.num = num;
+  }
+  /********************************************************* the get methods ***********************************************/
+  public boolean getCondition(){
+    return this.condition;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
+  
+  public String getDescr(){
+    return this.descr;
+  }
+  
+  public String getNum(){
+    return this.num;
+  }
+  
+  public String getStudent(){
+    return this.stu;
+  }
+  
+  public String getDate(){
+    return this.dueDate;
+  }
+  /********************************************************* the set methods ***********************************************/
+  
+  public void setStudent(String student){
+    this.stu = student;
+  }
+  
 }
