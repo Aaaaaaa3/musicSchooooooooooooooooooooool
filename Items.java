5ce46f30da7a906ad25ef45ccef2f1c31a0705a6
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-04-30 10:44:25 -0400

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,15 @@
+/**
+ * Template for insturments, equipment and sheet music
+ */
+public abstract class Items {
+  boolean condition;
+  String name;
+  String descr;
+  String num;
+  
+  String stu = "";
+  String dueDate;//change into appropriate thing later
+  
+  public Items() { 
+    `}
+}
