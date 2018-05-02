1fba275c4b0203e5d25d4c8547258da0e39c6376
chesyrin <chesyrin@users.noreply.github.com>
2018-05-01 23:15:01 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,66 +1,64 @@
-//gui for project
-//rename variables
-//card layout
+/* tabbed pane
+ *
+ */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GUI implements ItemListener{
-  JFrame frame = new JFrame("Music Resource Sign Out"); //new frame
-  JPanel cards = new JPanel(new CardLayout());
-  final String INSTRUMENTS = "Instrument"; //the options in the drop down menu
-  final String SHEETS = "Sheet Music";
-  final String EQUIPMENT = "Equipment";
-  JPanel comboBoxPane = new JPanel();
-  
-  public void itemStateChanged(ItemEvent evt) {
-    CardLayout cl = (CardLayout)(cards.getLayout());
-    cl.show(cards, (String)evt.getItem());
+public class GUI extends JPanel{
+  public GUI(){ //constructor
+    super(new GridLayout(1, 1));
+    JTabbedPane tabbedPane = new JTabbedPane();
+    JPanel instrumentPanel = new JPanel();
+    JPanel sheetPanel = new JPanel();
+    JPanel equipmentPanel = new JPanel();
+    
+    JTextField firstName1 = new JTextField("First name");
+    JTextField lastName1 = new JTextField("Last name");
+    JTextField firstName2 = new JTextField("First name");
+    JTextField lastName2 = new JTextField("Last name");
+    JTextField firstName3 = new JTextField("First name");
+    JTextField lastName3 = new JTextField("Last name");
+    JTextField instrument = new JTextField("Instrument");
+    JTextField sheetMusic = new JTextField ("Sheet Music");
+    JTextField equipment = new JTextField ("Equipment");
+    JTextField condition1 = new JTextField("Condition");
+    JTextField condition2 = new JTextField("Condition");
+    JTextField condition3 = new JTextField("Condition");
+    JButton signOut1 = new JButton ("Sign Out");
+    JButton signOut2 = new JButton ("Sign Out");
+    JButton signOut3 = new JButton ("Sign Out");
+    
+    instrumentPanel.add(firstName1);
+    instrumentPanel.add(lastName1);
+    instrumentPanel.add(instrument);
+    instrumentPanel.add(condition1);
+    instrumentPanel.add(signOut1);
+    
+    sheetPanel.add(firstName2);
+    sheetPanel.add(lastName2);
+    sheetPanel.add(sheetMusic);
+    sheetPanel.add(condition2);
+    sheetPanel.add(signOut2);
+    
+    equipmentPanel.add(firstName3);
+    equipmentPanel.add(lastName3);
+    equipmentPanel.add(equipment);
+    equipmentPanel.add(condition3);
+    equipmentPanel.add(signOut3);
+    
+    tabbedPane.addTab("Instrument", instrumentPanel);
+    tabbedPane.addTab("Sheet Music", sheetPanel);
+    tabbedPane.addTab("Equipment", equipmentPanel);
+    
+    add(tabbedPane); //add tabbed pane to this panel
   }
-      
-  public void main(String[] args) { 
-    cards.setPreferredSize(new Dimension(300, 500));
-    comboBoxPane.setSize(100, 100);
-    
-    //drop down menu
-    String comboBoxItems[] = {INSTRUMENTS, SHEETS, EQUIPMENT};
-    JComboBox cb = new JComboBox(comboBoxItems);
-    cb.addItemListener(this);
-    comboBoxPane.add(cb);
-    
-    JPanel card1 = new JPanel(new GridLayout(2,4));
-    card1.add(new JLabel("First name:"));
-    card1.add(new JTextField());
-    card1.add(new JLabel("Last name:"));
-    card1.add(new JTextField());
-    card1.add(new JLabel("Instrument:"));
-    card1.add(new JTextField());
-    card1.add(new JLabel("Condition"));
-    card1.add(new JTextField());
-    
-    JPanel card2 = new JPanel(new GridLayout(2,4));
-    card2.add(new JLabel("First name:"));
-    card2.add(new JTextField());
-    card2.add(new JLabel("Last name:"));
-    card2.add(new JTextField());
-    card2.add(new JLabel("Instrument:"));
-    card2.add(new JTextField());
-    card2.add(new JLabel("Condition"));
-    card2.add(new JTextField());
-    JPanel card3 = new JPanel();
-    
-    //Create the panel that contains the cards.
-    cards.add(card1, INSTRUMENTS);
-    cards.add(card2, SHEETS);
-    cards.add(card3, EQUIPMENT);
-    //add panels onto frame
-    frame.add(cards,BorderLayout.CENTER);
-    frame.add(comboBoxPane, BorderLayout.PAGE_START);
-    
-    //set to visible
-    frame.setVisible (true);
-    
+  
+  public static void main(String [] args){
+    JFrame frame = new JFrame ("Music Sign Out");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(500,500);
-  }  
+    frame.add(new GUI(), BorderLayout.CENTER);
+    frame.setSize(400,300);
+    frame.setVisible(true);
+  }
 }
