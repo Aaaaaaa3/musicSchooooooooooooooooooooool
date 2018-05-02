01c0aa333c692ef2d4539af3594d17fe837d503e
chesyrin <chesyrin@users.noreply.github.com>
2018-05-02 13:34:36 -0400

    Add files via upload

--- Equipment.java
+++ Equipment.java
@@ -5,8 +5,45 @@
  * For the equipment objects
  */
 public class Equipment {
-  String name = "none";
-  int number = -1;
-  boolean goodCondition = true;
-  String description = "n/a"; //what is a description????
+  private String name = "none";
+  private int number = -1;
+  private boolean goodCondition = true;
+  private String description = "n/a"; //what is a description????
+  
+  public Equipment (){
+  }
+  
+  //methods to get info
+  public String getName(){
+    return this.name;
+  }
+  
+  public int getNum(){
+    return this.number;
+  }
+  
+  public boolean returnCondition(){
+    return goodCondition;
+  }
+  
+  public String getDescription(){
+    return this.description;
+  }
+  
+  //methdos to set info
+  public void setName(String name){
+    this.name=name;
+  }
+  
+  public void setNum(int num){
+    this.number=num;
+  }
+  
+  public void setCondition(boolean condition){
+    this.goodCondition=condition;
+  }
+  
+  public void setDescription(String desc){
+    this.description=desc;
+  }
 }
