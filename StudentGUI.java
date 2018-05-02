c68247fa68a9eecfcdabd1b6214e16bf6ec5278b
chesyrin <chesyrin@users.noreply.github.com>
2018-05-02 11:15:45 -0400

    Add files via upload

---
+++ StudentGUI.java
@@ -0,0 +1,68 @@
+/* StudentGUI.java
+ * Julia Zhao and Tasha Xiao
+ * May 02 2018 
+ * Version 1.0.0
+ * GUI program for the student
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class StudentGUI extends JPanel{
+  public StudentGUI(){ //constructor
+    super(new GridLayout(1, 1));
+    JTabbedPane tabbedPane = new JTabbedPane();
+    JPanel instrumentPanel = new JPanel();
+    JPanel sheetPanel = new JPanel();
+    JPanel equipmentPanel = new JPanel();
+    
+    JTextField firstName1 = new JTextField("First name");
+    JTextField lastName1 = new JTextField("Last name");
+    JTextField firstName2 = new JTextField("First name");
+    JTextField lastName2 = new JTextField("Last name");
+    JTextField firstName3 = new JTextField("First name");
+    JTextField lastName3 = new JTextField("Last name");
+    JTextField instrument = new JTextField("Instrument");
+    JTextField sheetMusic = new JTextField ("Sheet Music");
+    JTextField equipment = new JTextField ("Equipment");
+    JCheckBox condition1 = new JCheckBox("Good condition?");
+    JCheckBox condition2 = new JCheckBox("Good condition?");
+    JCheckBox condition3 = new JCheckBox("Good condition?");
+    JButton signOut1 = new JButton ("Sign Out");
+    JButton signOut2 = new JButton ("Sign Out");
+    JButton signOut3 = new JButton ("Sign Out");
+    
+    instrumentPanel.add(firstName1);
+    instrumentPanel.add(lastName1);
+    instrumentPanel.add(instrument);
+    instrumentPanel.add(condition1);
+    instrumentPanel.add(signOut1);
+    
+    sheetPanel.add(firstName2);
+    sheetPanel.add(lastName2);
+    sheetPanel.add(sheetMusic);
+    sheetPanel.add(condition2);
+    sheetPanel.add(signOut2);
+    
+    equipmentPanel.add(firstName3);
+    equipmentPanel.add(lastName3);
+    equipmentPanel.add(equipment);
+    equipmentPanel.add(condition3);
+    equipmentPanel.add(signOut3);
+    
+    tabbedPane.addTab("Instrument", instrumentPanel);
+    tabbedPane.addTab("Sheet Music", sheetPanel);
+    tabbedPane.addTab("Equipment", equipmentPanel);
+    
+    add(tabbedPane); 
+  }
+  
+  public static void run(){
+    JFrame frame = new JFrame ("Music Sign Out");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new StudentGUI(), BorderLayout.CENTER);
+    frame.setSize(400,300);
+    frame.setVisible(true);
+  }
+}
---
+++ TeacherGUI.java
@@ -0,0 +1,9 @@
+/* TeacherGUI.java
+ * Julia Zhao and Tasha Xiao
+ * May 02 2018
+ * Version 1.0.0
+ * GUI program for the teacher
+ */
+public class TeacherGUI {
+
+}
