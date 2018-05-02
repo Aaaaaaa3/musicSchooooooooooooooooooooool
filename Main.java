6d1319785167fdf6d5ccedaca716f4af9dd44acf
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-02 11:16:37 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
  * Read in the file
  * Add and remove students
  * 
- * Finished: txt file read in (not yet saved to list)
+ * Finished: txt file read in & saved to list
  */
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,9 +10,10 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-
+  
   public static void main(String[] args) { 
     
+    SimpleLinkedList<Items> list = new SimpleLinkedList<Items>();
     try {
       File file = new File("Tasha.txt");
       Scanner fileInput = new Scanner(file);
@@ -30,16 +31,23 @@ public class Main {
           tempItem.setPerson(tempStu);
           
           SimpleDateFormat tempDate = new SimpleDateFormat(stringInput.next());
+          
           tempItem.setDate(tempDate);
         }
         //set description
-         tempItem.setDescr(temp.substring(temp.indexOf('"')+1));       
-        tempItem.display();
-        System.out.println();
+        tempItem.setDescr(temp.substring(temp.indexOf('"')+1));       
+        list.add(tempItem);
+        
       }
     }
     catch(Exception E){
       System.out.println("Error 1");
+    }
+    
+    for (int i = 0; i< list.size(); i++){
+      Items temp = list.get(i);
+      temp.display();
+      System.out.println();
     }
   }
   
