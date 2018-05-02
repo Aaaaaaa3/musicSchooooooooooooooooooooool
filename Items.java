1a603cb9aa560546c2f8c4d10335b32907ae4329
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-02 10:54:22 -0400

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,26 +1,41 @@
-
 /**
  * Template for insturments, equipment and sheet music
  */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Items {
   private boolean condition;
   private String name;
-  private String descr;
+  private String descr = "";
   private String num;
   
   private Person stu;
-  private String dueDate;//change into appropriate thing later
+  private SimpleDateFormat dueDate;//change into appropriate thing later
   
   /********************************************************* the constructors ***********************************************/
   //default constructor
   public Items() { 
-    }
+  }
   
-  public Items(boolean condition, String name, String description, String num){
+  public Items(String name, boolean condition, String num){
     this.condition = condition;
     this.name = name;
-    this.descr = description;
     this.num = num;
+  }
+  
+  public void display(){
+    
+    System.out.println("Name: " + this.name);
+    System.out.println("Num: " + this.num);
+    System.out.println("Condition: " + this.condition);
+    System.out.println("Description: "+ this.descr);
+    if(this.stu != null){
+      System.out.println("Student: " + this.stu.name);//getName());
+      Date date = new Date();
+      System.out.println("Due date: " + this.dueDate.format(date));
+    }
+    
   }
   /********************************************************* the get methods ***********************************************/
   public boolean getCondition(){
@@ -43,13 +58,21 @@ public class Items {
     return this.stu;
   }
   
-  public String getDate(){
+  public SimpleDateFormat getDate(){
     return this.dueDate;
   }
   /********************************************************* the set methods ***********************************************/
   
   public void setPerson(Person student){
     this.stu = student;
+  }
+  
+  public void setDescr(String descr){
+    this.descr = descr;
+  }
+  
+  public void setDate(SimpleDateFormat date){
+    this.dueDate = date;
   }
   
 }
