37254ed7b41de01b834e418328ade9367d096327
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-02 10:55:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,41 @@
 /**
  * Read in the file
  * Add and remove students
+ * 
+ * Finished: txt file read in (not yet saved to list)
  */
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-  
-  
+
   public static void main(String[] args) { 
     
     try {
       File file = new File("Tasha.txt");
-      Scanner input = new Scanner(file);
+      Scanner fileInput = new Scanner(file);
       
-      while (input.hasNext()){
-        System.out.println(input.nextLine());
+      while (fileInput.hasNext()){
+        String temp = fileInput.nextLine();
+        Scanner stringInput = new Scanner(temp);
+        
+        Items tempItem = new Items(stringInput.next(), stringInput.nextBoolean(),
+                                   stringInput.next());
+        String tempTemp = stringInput.next();
+        //if it has a student
+        if (tempTemp.charAt(0) != '"'){
+          Person tempStu = new Person(tempTemp);
+          tempItem.setPerson(tempStu);
+          
+          SimpleDateFormat tempDate = new SimpleDateFormat(stringInput.next());
+          tempItem.setDate(tempDate);
+        }
+        //set description
+         tempItem.setDescr(temp.substring(temp.indexOf('"')+1));       
+        tempItem.display();
+        System.out.println();
       }
     }
     catch(Exception E){
@@ -24,3 +44,6 @@ public class Main {
   }
   
 }
+/*Txt file format:
+ * Name condition(0/1) number description student dueDate
+ */
