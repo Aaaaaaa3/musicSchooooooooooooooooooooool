f891923d9e88900661bc364ae3662a95ffd46e0c
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-02 15:28:54 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -5,7 +5,8 @@ public class Person {
   
   boolean status;//whether the person is a student or a teacher
   String name;
-  public Person() { 
+   public Person(String name) {
+    this.name = name;
   }
   
 }
