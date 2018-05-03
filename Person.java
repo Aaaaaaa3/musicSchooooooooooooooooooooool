185add6e91f641e33c574e9f2f21ac0883ab9862
chesyrin <chesyrin@users.noreply.github.com>
2018-05-03 11:17:35 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,22 +1,33 @@
-/**
- * Auto Generated Java Class.
- */
 public class Person {
   
   private boolean status;//whether the person is a student or a teacher
   private String name = "";
   private int personNum;
   
+  public Person (){
+  }
+  
   public Person(String name, int num) {
     this.name = name;
     this.personNum = num;
   }
-    public Person(String name) {
+  public Person(String name) {
     this.name = name;
   }
-    
-    public String getName(){
-      return this.name;
-    }
   
+  public String getName(){
+    return this.name;
+  }
+  
+  public int getNum(){
+    return personNum;
+  }
+  
+  public void setNum (int num){
+    personNum = num;
+  }
+  
+  public void setName(String name){
+    name=name;
+  }
 }
