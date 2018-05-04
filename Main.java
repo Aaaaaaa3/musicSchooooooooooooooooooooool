cfd40551ef88d53ed2ce8f3b9f62ef27ae7716b4
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-04 09:43:06 -0400

    Update Main.java
    For now I added in a constant student number/date if you want to sign out, but we need to change that
    and I added in the student number as a String under "name"

--- Main.java
+++ Main.java
@@ -11,11 +11,31 @@ import java.util.Scanner;
 
 public class Main {
   
+    public Person checkStudent (int num, DoubleLinkedList<Person> students){
+    int index=0;
+    while (index < students.size()){
+      if (num==students.get(index).getNum()){
+        return students.get(index);
+      }
+      index++;
+    }
+    return null;
+  }
+  
+    public static Items checkItem (String input, DoubleLinkedList<Items> list){
+      for (int i = 0; i<list.size(); i++){
+      if (input.equals(list.get(i).getName())){
+        return list.get(i);
+      }
+    }
+    return null;
+  }
+  
   public static void main(String[] args) { 
     
     SimpleLinkedList<Items> list = new SimpleLinkedList<Items>();
     
-    //read in the music file
+//read in the music file----------------------------------
     try {
       File file = new File("Tasha.txt");
       Scanner fileInput = new Scanner(file);
@@ -32,9 +52,7 @@ public class Main {
           Person tempStu = new Person(tempTemp);
           tempItem.setPerson(tempStu);
           
-          SimpleDateFormat tempDate = new SimpleDateFormat(stringInput.next());
-          
-          tempItem.setDate(tempDate);
+          tempItem.setDate(stringInput.next());
         }
         //set description
         tempItem.setDescr(temp.substring(temp.indexOf('"')+1));       
@@ -46,10 +64,33 @@ public class Main {
       System.out.println("Error 1");
     }
     
+//------------------------------------------------------------------------
     for (int i = 0; i< list.size(); i++){
       Items temp = list.get(i);
       temp.display();
       System.out.println();
+    }
+
+    Scanner input = new Scanner(System.in);
+    while (true){
+      System.out.println("Press 1 to sign out, press 2 to sign in");
+      String temp = input.nextLine();
+      if (temp.equals("1")){
+        System.out.println("What do you want to sign out?");
+        temp = input.nextLine();
+        Items tempTemp = checkItem(temp, list);
+        Person tempStu = new Person("312106534645345315");
+        tempTemp.setPerson(tempStu);
+        tempTemp.setDate("2018/6/3");
+        
+            for (int i = 0; i< list.size(); i++){
+      Items temp3 = list.get(i);
+      temp3.display();
+      System.out.println();
+    }
+      }
+      else if (temp.equals("2")){
+      }
     }
   }
   
