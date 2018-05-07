f8e93907cd0f238303302abcc6a5b031925b5c9f
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-07 13:54:48 -0400

    Update Recent.java

--- Recent.java
+++ Recent.java
@@ -5,17 +5,16 @@ import javax.swing.*;
 public class Recent {
   public Recent(){
     JFrame frame = new JFrame ("Music Sign Out");
-    frame.setSize(400,300);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300,400);
     JPanel panel = new JPanel ();
     panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-    JLabel label = new JLabel ("Recent sign outs:");
+    JLabel label = new JLabel ("\tRecent Sign Outs:");
     DoubleLinkedList<Items> list = MusicResource.getItems();
     JLabel signOuts[] = new JLabel[list.size()];
     
     for(int i = 0; i<list.size(); i++){
       if (list.get(i).getPerson() != -1){
-      signOuts[i] = new JLabel(list.get(i).getName() + 
+      signOuts[i] = new JLabel("\t" + list.get(i).getName() + 
                                "\t Due: " + list.get(i).getDate() + 
                                "\t Name: " + list.get(i).getPerson());
       }
