efa719ec20699da00e2663ef450c848596053bac
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-08 12:14:48 -0400

    Update Edit.java

--- Edit.java
+++ Edit.java
@@ -7,37 +7,39 @@ public class Edit extends JPanel implements ActionListener{
   JPanel panel2 = new JPanel();
   JButton ok = new JButton("OK");
   JButton cancel = new JButton("Cancel");
+  Items tempItem = new Items();
+  
+  JLabel nameLab = new JLabel("Name: ");
+  JTextField name = new JTextField(10);
+  JLabel descrLab = new JLabel("Description: ");
+  JTextField descr = new JTextField(10);
+  JLabel conditionLab = new JLabel("Condition: ");
+  JTextField condition = new JTextField(10);
+  JLabel numLab = new JLabel("Number: ");
+  JTextField num = new JTextField(10);
   public Edit(){
     
     DoubleLinkedList<Items> list = MusicResource.getItems();
     
-    
-    
     frame2.setVisible(false);
     frame2.setSize(500,500);
     
-    
     String instName = JOptionPane.showInputDialog(frame2, "What insturment?");
     
-    Items tempItem = MusicResource.checkItem(instName, list);
+    tempItem = MusicResource.checkItem(instName, list);
     if (tempItem == null){
       JOptionPane.showMessageDialog(null, "Not found");
     }
     else {
-      JLabel nameLab = new JLabel("Name: ");
-      JTextField name = new JTextField(tempItem.getName(), 10);
-      JLabel descrLab = new JLabel("Description: ");
-      JTextField descr = new JTextField(tempItem.getDescr(), 10);
-      JLabel conditionLab = new JLabel("Condition: ");
-      JTextField condition = new JTextField(10);
+      name = new JTextField(tempItem.getName(), 10);
+      descr = new JTextField(tempItem.getDescr(), 10);
       if(tempItem.getCondition() == true){
         condition = new JTextField("OK", 10);
       }
       else {
         condition = new JTextField("Not OK", 10);
       }
-      JLabel numLab = new JLabel("Number: ");
-      JTextField num = new JTextField(tempItem.getNum(), 10);
+      num = new JTextField(tempItem.getNum(), 10);
       
       ok.addActionListener(this);
       cancel.addActionListener(this);
@@ -62,12 +64,21 @@ public class Edit extends JPanel implements ActionListener{
   
   public void actionPerformed(ActionEvent e){
     if (e.getSource()== ok){
-      System.out.println("1");
+      tempItem.setName(name.getText());
+      tempItem.setDescr(descr.getText());
+      String cond = condition.getText();
+      if (cond.equals("OK")){
+        tempItem.setCondition(true);
+      }
+      else if (cond.equals("Not OK")){
+        tempItem.setCondition(false);
+      }
+      tempItem.setNum(num.getText());
+
     }
-    else{
-      this.setVisible(false);
-      frame2.dispose();
-    }
+    
+    this.setVisible(false);
+    frame2.dispose();
     
   }
 }
