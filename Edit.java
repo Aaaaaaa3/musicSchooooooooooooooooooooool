b5a469207454a427e85675ff3c145adae98a5ee1
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-08 12:22:39 -0400

    Update Edit.java

--- Edit.java
+++ Edit.java
@@ -26,40 +26,42 @@ public class Edit extends JPanel implements ActionListener{
     
     String instName = JOptionPane.showInputDialog(frame2, "What insturment?");
     
-    tempItem = MusicResource.checkItem(instName, list);
-    if (tempItem == null){
-      JOptionPane.showMessageDialog(null, "Not found");
-    }
-    else {
-      name = new JTextField(tempItem.getName(), 10);
-      descr = new JTextField(tempItem.getDescr(), 10);
-      if(tempItem.getCondition() == true){
-        condition = new JTextField("OK", 10);
+    if (instName != null){
+      tempItem = MusicResource.checkItem(instName, list);
+      if (tempItem == null){
+        JOptionPane.showMessageDialog(null, "Not found");
       }
       else {
-        condition = new JTextField("Not OK", 10);
+        name = new JTextField(tempItem.getName(), 10);
+        descr = new JTextField(tempItem.getDescr(), 10);
+        if(tempItem.getCondition() == true){
+          condition = new JTextField("OK", 10);
+        }
+        else {
+          condition = new JTextField("Not OK", 10);
+        }
+        num = new JTextField(tempItem.getNum(), 10);
+        
+        ok.addActionListener(this);
+        cancel.addActionListener(this);
+        
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+        panel2.add(nameLab);
+        panel2.add(name);
+        panel2.add(descrLab);
+        panel2.add(descr);
+        panel2.add(conditionLab);
+        panel2.add(condition);
+        panel2.add(numLab);
+        panel2.add(num);
+        panel2.add(ok);
+        panel2.add(cancel);
+        
+        frame2.add(panel2);
+        frame2.setVisible(true);
       }
-      num = new JTextField(tempItem.getNum(), 10);
       
-      ok.addActionListener(this);
-      cancel.addActionListener(this);
-      
-      panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
-      panel2.add(nameLab);
-      panel2.add(name);
-      panel2.add(descrLab);
-      panel2.add(descr);
-      panel2.add(conditionLab);
-      panel2.add(condition);
-      panel2.add(numLab);
-      panel2.add(num);
-      panel2.add(ok);
-      panel2.add(cancel);
-      
-      frame2.add(panel2);
-      frame2.setVisible(true);
     }
-    
   }
   
   public void actionPerformed(ActionEvent e){
@@ -74,7 +76,7 @@ public class Edit extends JPanel implements ActionListener{
         tempItem.setCondition(false);
       }
       tempItem.setNum(num.getText());
-
+      
     }
     
     this.setVisible(false);
