3ec807234ae1ee794d4447a6140bfa3a164ebb0d
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-09 18:13:11 -0400

    Update RecentStudent.java

--- RecentStudent.java
+++ RecentStudent.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.text.SimpleDateFormat;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -10,10 +12,12 @@ public class RecentStudent {
     JPanel panel = new JPanel ();
     panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
     JLabel label = new JLabel ("Recent Sign Outs:");
+    JLabel overdue = new JLabel("----Overdue:---");
     
     String stuNum = JOptionPane.showInputDialog(frame2, "What's your name?");
 
     DoubleLinkedList<Items> list = MusicResource.getItems();
+    DoubleLinkedList.sortChrono(list);
     JLabel signOuts[] = new JLabel[list.size()];
     
     for(int i = 0; i<list.size(); i++){
@@ -26,11 +30,23 @@ public class RecentStudent {
     
     
     panel.add(label);
-    for (int i = 0; i<list.size(); i++){
-      if(signOuts[i] != null){
-      panel.add(signOuts[i]);
-      }
+    
+    String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date());
+    int j = 0;
+    
+    while (j<list.size() && (signOuts[j] != null) && list.get(j).getDate().compareTo(currentDate)>=0){
+      panel.add(signOuts[j]);
+      j++;
     }
+    
+    panel.add(overdue);
+    
+    while (j<list.size() && (signOuts[j] != null) && list.get(j).getPerson() != -1){
+      panel.add(signOuts[j]);
+      j++;
+      System.out.println(j);
+    }
+
     frame.add(panel);
     frame.setVisible(true);
     
