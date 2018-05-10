ee83095976865f43700953aafe790576d54a1735
Aaaaaaa3 <Tasha.xiao@gmail.com>
2018-05-10 00:03:50 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,9 +1,16 @@
+/* Person.java
+ * Julia Zhao and Tasha Xiao
+ * May 03 2018
+ * Version 1.0.0
+ * Template for a person
+ */
 public class Person {
   
   private boolean status;//whether the person is a student or a teacher
-  private String name = "";
-  private int personNum;
+  private String name = "";//the name of the person
+  private int personNum;//their student number
   
+  //default constructor
   public Person (){
   }
   
@@ -11,23 +18,35 @@ public class Person {
     this.name = name;
     this.personNum = num;
   }
-    public Person(String num) {
+  public Person(String num) {
     this.personNum = Integer.parseInt(num);
   }
-  
+  /* getName
+   * gets the person's name
+   * @return      the person's name
+   */
   public String getName(){
     return this.name;
   }
-  
+  /* getNum
+   * gets the person's number (student/teacher number)
+   * @return      the person's number
+   */
   public int getNum(){
-    return personNum;
+    return this.personNum;
   }
-  
+  /* getNum
+   * sets the person's number
+   * @param      the number to be set
+   */
   public void setNum (int num){
     personNum = num;
   }
-  
+  /* setName
+   * sets the person's name
+   * @param      the person's name
+   */
   public void setName(String name){
-    name=name;
+    this.name=name;
   }
-}
+}//end of Person.java
